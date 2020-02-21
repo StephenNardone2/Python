db146366d20d0741c9be32a2fe8a89a87a90c659
Stephen P Nardone II <60292003+StephenNardone2@users.noreply.github.com>
2020-02-20 23:26:13 -0500

    Lots lol

--- scraper.py
+++ scraper.py
@@ -1,25 +1,9 @@
-''' Important:
-        You will either need easy_install or pip to
-        install the mechanize library. Mechanize will
-        be discontinuing support for Python 2.X also.
-'''
-import mechanize 
+from anonBrowser import *
 
-# Retrieving a website's source code
-'''def viewPage(url):
-    browser = mechanize.Browser()
-    page = browser.open(url)
-    source_code = page.read()
-    print source_code
-viewPage('http://www.syngress.com/')
-'''
-# Anonymity - Adding Proxy
-def testProxy(url, proxy):
-    browser = mechanize.Browser()
-    browser.set_proxies(proxy)
-    page = browser.open(url)
-    source_code = page.read()
-    print (source_code)
-url = 'http://ip.nefsc.noaa.gov'
-hideMeProxy = {'http': '209.141.46.133:8080'}
-testProxy(url, hideMeProxy)
+ab = anonBrowser(proxies=[], user_agents=[('User-agent', 'superSecretBrowser')])
+for attempt in range(1, 5):
+    ab.anonymize()
+    print('[*] Fetching page')
+    response = ab.open('http://kittenwar.com')
+    for cookie in ab.cookie_jar:
+        print(cookie)
